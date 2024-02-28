a8ff0559563d7e7a79fc8286a733099340aed1fe
Bilal <bilalk6789@yahoo.com>
2024-02-27 19:25:32 -0600

    save

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,62 +6,60 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html', // html temp
-        filename: 'index.html', 
-        chunks: ['main'], 
-      }),
-      new WebpackPwaManifest({
-        filename: 'manifest.json',
-        name: 'J.A.T.E',
-        short_name: 'JATE',
-        description: 'Just Another Text Editor',
-        start_url: '/',
-        background_color: '#ffffff',
-        theme_color: '#31a9e1',
-        icons: [
-          {
-            src: path.resolve('src/assets/icons/icon_96x96.png'),
-            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
-            destination: path.join('icons'),
-          },
-        ],
-      }),
-      new InjectManifest({
-        swSrc: './src-sw.js', // sw file
-        swDest: 'service-worker.js', 
-      }),
-    ],
-    module: {
-      rules: [
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/js/index.js',
+    install: './src/js/install.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'src'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      chunks: ['main'],
+    }),
+    new WebpackPwaManifest({
+      filename: 'manifest.json',
+      name: 'J.A.T.E',
+      short_name: 'JATE',
+      description: 'Just Another Text Editor',
+      start_url: '/',
+      background_color: '#ffffff',
+      theme_color: '#31a9e1',
+      icons: [
         {
-          test: /\.css$/, // css file
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.js$/, // babel
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread'],
-            },
-          },
+          src: path.resolve('src/assets/icons/icon_96x96.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('icons'),
         },
       ],
-    },
-  };
+    }),
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'service-worker.js',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread'],
+          },
+        },
+      },
+    ],
+  },
 };
